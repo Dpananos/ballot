985a58b077bbf5e264828d506cdecdddf0817379
Demetri Pananos <demetri.pananos@zapier.com>
2022-11-09 11:40:10 -0500

    edits main to execute

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-import hydra
-from omegaconf import OmegaConf
-import logging
 import pandas as pd
 import numpy as np
-from src.experiment import Experiment
-from src.models import dummy_classifier, naive_bayes_classifier
+
+import logging
+import hydra
+from omegaconf import OmegaConf
+
+from sklearn.dummy import DummyClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder
 
 
 def voting_experiment(cfg) -> None:
@@ -13,21 +19,31 @@ def voting_experiment(cfg) -> None:
     X = df[cfg.dataset.features].values.reshape(-1, 1)
     y = df[cfg.dataset.target].values
 
+    Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, train_size=0.5, random_state=0)
+
     # Load in the untrained models
     if cfg.model.name == "Naive Bayes":
-        model = naive_bayes_classifier()
+
+        model = Pipeline([
+            ('ohe', OneHotEncoder(sparse=False, handle_unknown='ignore')),
+            ('nb', MultinomialNB())
+        ])
+                
     elif cfg.model.name == "Dummy Classifier":
-        model = dummy_classifier()
+        model = DummyClassifier(strategy="uniform")
+
     else:
         raise ValueError("Not Implemented")
 
     # Run experiments
-    experiment = Experiment(model=model, X=X, y=y)
+    model.fit(Xtrain, ytrain)
+    ypred = model.predict(Xtest)
+    score = accuracy_score(ytest, ypred)
+    
     logging.info("Experiment Results")
     logging.info(f"Model = {cfg.model.name}")
-    logging.info('\n' + experiment.report)
-        
-
+    logging.info(f'Accuracy Score: {score:.2f}')
+    
     return None
 
 
